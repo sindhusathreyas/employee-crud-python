abb06bbf0c8735c6a7ebefaa126a4db8f93965a1
sindhusathreyas <https://github.com/sindhusathreyas>
2026-09-21 13:00:45 +0530

    Prepare Flask app for Render deployment

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ def delete_employee(id):
 # -------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
