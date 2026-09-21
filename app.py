3e98387147e8ac3a83f9dccac63adf1edcca1685
sindhusathreyas <https://github.com/sindhusathreyas>
2026-09-21 12:51:04 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from db import get_db_connection
+import os
 
 
 app = Flask(__name__)
@@ -200,4 +201,4 @@ def delete_employee(id):
 # -------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT", 3306))
     )
     return connection
